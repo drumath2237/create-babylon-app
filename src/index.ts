9037ab5d00482701c6a442ead5bd1d5a7b8c5bdc
drumath <2237k.drumath@gmail.com>
2023-06-10 18:14:34 +0900

    :sparkles: test inquirer prompt

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,27 @@
-console.log('Hello');
+import { input, select } from '@inquirer/prompts';
+
+const main = async (): Promise<void> => {
+  const formatProjectName = (name: string) => name.trim();
+
+  console.log(typeof formatProjectName === 'function');
+
+  const projectName = await input({
+    message: 'Project Name:',
+    default: 'Babylon-App',
+    validate: (name) => name !== '',
+  });
+
+  const buildTool = await select({
+    message: 'Build Tools:',
+    choices: [{ value: 'Vite' }, { value: 'Webpack' }],
+  });
+
+  const language = await select({
+    message: 'Language:',
+    choices: [{ value: 'TypeScript' }, { value: 'JavaScript' }],
+  });
+
+  console.log(projectName, buildTool, language);
+};
+
+main();
