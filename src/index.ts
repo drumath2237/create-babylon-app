6b6985b2a21557fee7b9be67bf90a2e649862059
drumath <2237k.drumath@gmail.com>
2023-06-11 23:50:00 +0900

    :sparkles: implement folder copy

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,43 @@
 import { input, select } from '@inquirer/prompts';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { mkdir } from 'fs/promises';
+import { copy } from 'fs-extra';
 
 const main = async (): Promise<void> => {
-  console.log('working directory: ', process.cwd());
-  console.log('url: ', import.meta.url);
+  const testTemplateDir = path.resolve(
+    fileURLToPath(import.meta.url),
+    '../..',
+    'templates/test'
+  );
 
-  // const projectName = await input({
-  //   message: 'Project Name:',
-  //   default: 'Babylon-App',
-  //   validate: (name) => name !== '',
-  // });
+  const projectName = await input({
+    message: 'Project Name:',
+    default: 'Babylon-App',
+    validate: (name) => name !== '',
+  });
 
-  // const buildTool = await select({
-  //   message: 'Build Tools:',
-  //   choices: [
-  //     { value: 'vite', name: 'Vite' },
-  //     { value: 'webpack', name: 'Webpack(not ready)', disabled: true },
-  //   ],
-  // });
+  const buildTool = await select({
+    message: 'Build Tools:',
+    choices: [
+      { value: 'vite', name: 'Vite' },
+      { value: 'webpack', name: 'Webpack(not ready)', disabled: true },
+    ],
+  });
 
-  // const language = await select({
-  //   message: 'Language:',
-  //   choices: [
-  //     { value: 'ts', name: 'TypeScript' },
-  //     { value: 'js', name: 'JavaScript' },
-  //   ],
-  // });
+  const language = await select({
+    message: 'Language:',
+    choices: [
+      { value: 'ts', name: 'TypeScript' },
+      { value: 'js', name: 'JavaScript' },
+    ],
+  });
 
-  // console.log(projectName, buildTool, language);
+  const workingDir = process.cwd();
+  const appDir = path.join(workingDir, projectName);
+
+  await mkdir(appDir);
+  await copy(testTemplateDir, appDir);
 };
 
 main();
