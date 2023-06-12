dcd6957e5a33efc63361dcff9c3cd6b48f60403b
drumath <2237k.drumath@gmail.com>
2023-06-12 22:53:57 +0900

    :sparkles: create vite-js template WIP

---
+++ templates/vite-js/main.js
@@ -0,0 +1,12 @@
+import './style.css';
+
+const main = () => {
+  const renderCanvas = document.getElementById('renderCanvas');
+  if (!renderCanvas) {
+    return;
+  }
+
+  console.log('Hello~~');
+};
+
+main();
