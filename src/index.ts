48fafa5a4902bb0066df4703e268e3410b8551f1
drumath2237 <2237k.drumath@gmail.com>
2024-07-20 18:19:55 +0900

    :bug: migrating to consola WIP

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,9 @@
-import { log } from "node:console";
 import { mkdir } from "node:fs/promises";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
-import { input, select } from "@inquirer/prompts";
 import { runMain as _runMain, defineCommand } from "citty";
 import { copy, readJSON, writeJson } from "fs-extra/esm";
+import { consola } from "consola";
 
 export const runMain = () => _runMain(mainCommand);
 
@@ -13,52 +12,47 @@ const mainCommand = defineCommand({
     description:
       "create-babylon-app is a CLI for scaffolding Babylon.js web application project from templates!",
   },
-  run: () => {
-    main();
+  run: async () => {
+    const projectName = await consola.prompt("Project Name", {
+      type: "text",
+      default: "Babylon-App",
+      initial: "Babylon-App",
+    });
+
+    const buildTool = await consola.prompt("Build Tools", {
+      type: "select",
+      options: [{ value: "vite", label: "Vite" }],
+    });
+
+    consola.info(`${buildTool.value}, ${buildTool.label}`);
+
+    const language = await consola.prompt("Language", {
+      type: "select",
+      options: [
+        { value: "ts", label: "TypeScript" },
+        { value: "js", label: "JavaScript" },
+      ],
+    });
+
+    const templateDir = path.resolve(
+      fileURLToPath(import.meta.url),
+      "../..",
+      "templates",
+      `${buildTool.value}-${language.value}`,
+    );
+
+    const workingDir = process.cwd();
+    const appDir = path.join(workingDir, projectName);
+
+    await mkdir(appDir);
+    await copy(templateDir, appDir);
+
+    const packageJsonPath = path.join(appDir, "package.json");
+    const packageJson = await readJSON(packageJsonPath);
+    packageJson.name = projectName;
+    await writeJson(packageJsonPath, packageJson, { spaces: "\t" });
+
+    console.log("\nDone!");
+    console.log(`  cd ${projectName}\n`);
   },
 });
-
-const main = async (): Promise<void> => {
-  const projectName = await input({
-    message: "Project Name:",
-    default: "Babylon-App",
-    validate: (name) => name !== "",
-  });
-
-  const buildTool = await select({
-    message: "Build Tools:",
-    choices: [
-      { value: "vite", name: "Vite" },
-      { value: "webpack", name: "Webpack(not ready)", disabled: true },
-    ],
-  });
-
-  const language = await select({
-    message: "Language:",
-    choices: [
-      { value: "ts", name: "TypeScript" },
-      { value: "js", name: "JavaScript" },
-    ],
-  });
-
-  const templateDir = path.resolve(
-    fileURLToPath(import.meta.url),
-    "../..",
-    "templates",
-    `${buildTool}-${language}`,
-  );
-
-  const workingDir = process.cwd();
-  const appDir = path.join(workingDir, projectName);
-
-  await mkdir(appDir);
-  await copy(templateDir, appDir);
-
-  const packageJsonPath = path.join(appDir, "package.json");
-  const packageJson = await readJSON(packageJsonPath);
-  packageJson.name = projectName;
-  await writeJson(packageJsonPath, packageJson, { spaces: "\t" });
-
-  log("\nDone!");
-  log(`  cd ${projectName}\n`);
-};
