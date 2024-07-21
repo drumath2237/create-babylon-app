11b5907d838ea5aeb4f8ae019f7ab44f0647ad1f
drumath2237 <2237k.drumath@gmail.com>
2024-07-21 12:24:39 +0900

    :sparkles: package json name rename by pkg types
    also, remove fs-extra

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,10 @@
-import { mkdir } from "node:fs/promises";
+import { mkdir, cp } from "node:fs/promises";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
 import { runMain as _runMain, defineCommand } from "citty";
 import { consola } from "consola";
 import { colorize } from "consola/utils";
-import { copy, readJSON, writeJson } from "fs-extra/esm";
-import { readPackageJSON } from "pkg-types";
+import { readPackageJSON, writePackageJSON } from "pkg-types";
 
 export const runMain = () => _runMain(mainCommand);
 
@@ -50,12 +49,14 @@ const mainCommand = defineCommand({
     const appDir = path.join(workingDir, projectName);
 
     await mkdir(appDir);
-    await copy(templateDir, appDir);
+    await cp(templateDir, appDir, { recursive: true });
 
-    const packageJsonPath = path.join(appDir, "package.json");
-    const packageJson = await readJSON(packageJsonPath);
-    packageJson.name = projectName;
-    await writeJson(packageJsonPath, packageJson, { spaces: "\t" });
+    const packageJson = await readPackageJSON(appDir);
+    if (packageJson.name) {
+      packageJson.name = projectName;
+      const jsonPath = path.resolve(appDir, "package.json");
+      await writePackageJSON(jsonPath, packageJson);
+    }
 
     console.log("\nDone!");
     console.log(`  cd ${projectName}\n`);
