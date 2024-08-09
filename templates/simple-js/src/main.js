88f0f9ced8d171b45e6effa9114198df99734ead
drumath <2237k.drumath@gmail.com>
2024-08-10 04:13:10 +0900

    :sparkles: add simple-js template

---
+++ templates/simple-js/src/main.js
@@ -0,0 +1,24 @@
+import { Engine, MeshBuilder, Scene, Vector3 } from "@babylonjs/core";
+import "./style.css";
+
+const main = () => {
+  const renderCanvas = document.querySelector("#renderCanvas");
+  if (!renderCanvas) {
+    return;
+  }
+
+  const engine = new Engine(renderCanvas);
+  const scene = new Scene(engine);
+
+  scene.createDefaultCameraOrLight(true, true, true);
+  scene.createDefaultEnvironment();
+
+  const box = MeshBuilder.CreateBox("box", { size: 0.5 });
+  box.position = new Vector3(0, 0.25, 0);
+
+  window.addEventListener("resize", () => engine.resize());
+  engine.runRenderLoop(() => scene.render());
+};
+
+main();
+
